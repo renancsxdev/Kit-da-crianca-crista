4bd6cc4847c5f59a33b9ce313aa2f3d48aa2dd11
Renan <renan.costa.dev18@gmail.com>
2025-05-06 13:36:12 -0300

    pixel adaptado layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,46 +15,36 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Meta Pixel Code */}
+        {/* Código do Meta Pixel */}
         <Script
-          id="meta-pixel"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              !function(f,b,e,v,n,t,s) {
-                if (f.fbq) return; n = f.fbq = function() {
-                  n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-                };
-                if (!f._fbq) f._fbq = n;
-                n.push = n;
-                n.loaded = !0;
-                n.version = '2.0';
-                n.queue = [];
-                t = b.createElement(e);
-                t.async = !0;
-                t.src = v;
-                s = b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t, s)
-              }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-              
-              fbq('init', '1155984316024839');
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1076100644071297');
               fbq('track', 'PageView');
             `,
           }}
         />
       </head>
       <body>
-        {/* Tag noscript para rastrear visitantes sem JavaScript */}
+        {children}
+        {/* NoScript para usuários sem JS */}
         <noscript>
           <img
             height="1"
             width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1155984316024839&ev=PageView&noscript=1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1076100644071297&ev=PageView&noscript=1"
           />
         </noscript>
-
-        {children}
       </body>
     </html>
   );
